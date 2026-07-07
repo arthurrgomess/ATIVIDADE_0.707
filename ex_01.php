22da4c43cc9793cb4c82868469eccbb5df339e40
arthurgomess <arthur_r_gomes@estudante.sesisenai.org.br>
2026-07-07 07:49:05 -0300

    add_ peguei do prof

--- ex_01.php
+++ ex_01.php
@@ -1,31 +1,21 @@
+<?php
 
-<!DOCTYPE HTML>
-<html lang = "pt-br">
-    <head>
-        <title> Exemplo</title>
-        <meta charset = "UTF-8">
-    </head>
-    <body>
-        <form action=calculadora.php" method="get" >
-            Primeiro Numero: <input name="num1" type="text" />
-            Segundo numero: <input name="num2" type="text" /> 
-            Operacao (quatro operações):  <input name="operacao" type="text" /> 
-            <input type="submit" value="Calcular" />     
-    </form> 
-    <?php
-        $a = $_GET "num1";
-        $b = $_GET "num2";
-        $op =$_GET "operacao";
-        $c = []
-        if($op == "soma")
-            $c = $a + $b;
-        else if($op == "subtracao")
-            $c = $a - $b;
-        else if($op == "multiplicacao")
-            $c = $a*$b;
-        else
-            $c = $a/$b;
-         echo "O resultado da operação é: $c";
-    ?>      
-    </body>
-</html>
+// (x² + y²) / (x + y)
+
+function calcularFormula($x, $y)
+{
+    if (($x + $y) == 0) {
+        return "Não é possível realizar a divisão por zero.";
+    }
+
+    $resultado = (pow($x, 2) + pow($y, 2)) / ($x + $y);
+
+    return $resultado;
+}
+
+$x = 10;
+$y = 5;
+
+echo "Valor de X: $x <br>";
+echo "Valor de Y: $y <br><br>";
+echo "Resultado: " . calcularFormula($x, $y);
